8988c51984c37911de4e43ad3491544cc538b8d5
Aleš Vomáčka <61730945+alesvomacka@users.noreply.github.com>
2026-01-08 10:49:25 +0100

    added get_categorical_vars

--- R/helpers.R
+++ R/helpers.R
@@ -352,3 +352,45 @@ check_input <- function(data, vars, group) {
 
   invisible(TRUE)
 }
+
+#' Get names of categorical variables from a data frame
+#'
+#' Extracts the names of factor variables from a data frame. Optionally includes
+#' character variables and allows exclusion of specific variable names.
+#'
+#' @param data A data frame to extract categorical variable names from.
+#' @param exclude A character vector of variable names to exclude from the results.
+#'   Default is `NULL` (no exclusions).
+#' @param include_char Logical. If `TRUE`, character variables are included in
+#'   addition to factor variables. Default is `FALSE`.
+#'
+#' @return A character vector of variable names.
+#'
+#' @examples
+#' # Get only factor variables
+#' get_categerical_vars(iris)
+#'
+#' # Include character variables
+#' get_categerical_vars(mtcars, include_char = TRUE)
+#'
+#' # Exclude specific variables
+#' get_categerical_vars(iris, exclude = c("Species"))
+#'
+#' @export
+get_categerical_vars <- function(data, exclude = NULL, include_char = FALSE) {
+  # Get names of factor variables
+  factor_vars <- names(data)[sapply(data, is.factor)]
+
+  # Optionally include character variables
+  if (include_char) {
+    char_vars <- names(data)[sapply(data, is.character)]
+    factor_vars <- c(factor_vars, char_vars)
+  }
+
+  # Exclude specified variables
+  if (!is.null(exclude)) {
+    factor_vars <- setdiff(factor_vars, exclude)
+  }
+
+  factor_vars
+}
